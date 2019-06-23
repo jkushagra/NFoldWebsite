5b0ee2b744d806010dbb6a013728b2cfb206c735
Kushagra Jalota <kush.jalota07@gmail.com>
2019-06-23 19:24:53 +0530

    new

--- js/js_main.js
+++ js/js_main.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
 $('.carousel').carousel({
     pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
 });
