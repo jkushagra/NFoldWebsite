0729b4e67e4e02bb7fd1b8ec58b90c95f0e35760
Kushagra Jalota <kush.jalota07@gmail.com>
2019-07-04 10:38:27 +0530

    JS update

--- js/js_main.js
+++ js/js_main.js
@@ -30,6 +30,16 @@ $(document).ready(function() {
         } // End if
     });
 
+    /* Open when someone clicks on the span element */
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
     $(window).scroll(function() {
         $(".slideanim").each(function() {
             var pos = $(this).offset().top;
